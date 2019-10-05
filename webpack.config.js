2a8ab4c803400591484cf65183219eb82f5a3d88
Lucky Fox <sovenkovmaksim@gmail.com>
2019-10-05 10:43:26 +0300

    adde phone block in to phone page

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
     minimizer: [new UglifyJsPlugin()]
   }
 }
+var nodeExternals = require('webpack-node-externals')
+module.exports = {
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()] // in order to ignore all modules in node_modules folder
+}
 module.exports = {
   mode: 'development',
   entry: './js/index.js',
